35a4e3abb7bc43ed19a24b77be1c27b9904ade20
Leonardo Montoya <leonardo.montoya@utec.edu.pe>
2024-11-26 23:41:53 -0500

    correccion lambda para buscar cine por verificacion

--- LambdaBuscarCine.py
+++ LambdaBuscarCine.py
@@ -6,56 +6,63 @@ def lambda_handler(event, context):
     try:
         print(event)
 
-        # Validar el token de autorización
-        token = event['headers'].get('Authorization', None)
-        if not token:
-            return {
-                'statusCode': 401,
-                'status': 'Unauthorized - Falta el token de autorización'
-            }
-
-        if isinstance(event['body'], str):
-            body = json.loads(event['body'])
+        if event.get('motivo', 'buscar') == 'verificar':
+            tenant_id = event['tenant_id']
+            departamento = event['departamento']
+            provincia = event['provincia']
+            distrito = event['distrito']
+        
         else:
-            body = event['body']
+            if isinstance(event['body'], str):
+                body = json.loads(event['body'])
+            else:
+                body = event['body']
+            
+            # Extraer valores del cuerpo
+        
+            tenant_id = body['tenant_id']
+            departamento = body.get('departamento', "") 
+            provincia = body.get('provincia', "")       
+            distrito = body.get('distrito', "") 
 
-        # Extraer valores del cuerpo
-        # Extraer valores del cuerpo con valores predeterminados para campos opcionales
-        tenant_id = body['tenant_id']
-        departamento = body.get('departamento', "") 
-        provincia = body.get('provincia', "")       
-        distrito = body.get('distrito', "")         
+            # Validar el token de autorización
+            token = event['headers'].get('Authorization', None)
+            if not token:
+                return {
+                    'statusCode': 401,
+                    'status': 'Unauthorized - Falta el token de autorización'
+                }
 
+        
+            lambda_name = os.environ['LAMBDA_VALIDAR_TOKEN']
+            lambda_client = boto3.client('lambda')
+            payload_string = json.dumps(
+                {
+                    "tenant_id": tenant_id,
+                    "token": token
+                    })
+            invoke_response = lambda_client.invoke(
+                FunctionName=lambda_name,
+                InvocationType='RequestResponse',
+                Payload=payload_string
+            )
+            response = json.loads(invoke_response['Payload'].read())
+            print(response)
+            if response['statusCode'] == 403:
+                return {
+                    'statusCode': 403,
+                    'status': 'Forbidden - Acceso NO Autorizado'
+                }
 
         tabla_cines = os.environ["TABLE_NAME_CINES"]
-        lambda_name = os.environ['LAMBDA_VALIDAR_TOKEN']
-
-        lambda_client = boto3.client('lambda')
-        payload_string = json.dumps(
-            {
-                "tenant_id": tenant_id,
-                "token": token
-                })
-
-        invoke_response = lambda_client.invoke(
-            FunctionName=lambda_name,
-            InvocationType='RequestResponse',
-            Payload=payload_string
-        )
-        response = json.loads(invoke_response['Payload'].read())
-        print(response)
-        if response['statusCode'] == 403:
-            return {
-                'statusCode': 403,
-                'status': 'Forbidden - Acceso NO Autorizado'
-            }
 
         # Conexión a DynamoDB
         dynamodb = boto3.resource('dynamodb')
         table = dynamodb.Table(tabla_cines)
 
         # Construir la clave de ordenamiento
-        ordenamiento_filter = f"{departamento}#{provincia}#{distrito}".strip("#")
+        ordenamiento_filter = f"{departamento}#{provincia}#{distrito}"
+        print(f"Ordenamiento construido: {ordenamiento_filter}")
 
         # Configurar las expresiones para el Query
         key_condition_expression = "tenant_id = :tenant_id"
@@ -71,12 +78,18 @@ def lambda_handler(event, context):
             ExpressionAttributeValues=expression_attribute_values
         )
 
-        # Retornar los resultados
-        return {
-            'statusCode': 200,
-            'status': 'Búsqueda exitosa',
-            'data': response.get('Items', [])
-        }
+        print(f"Respuesta de DynamoDB: {response}")
+
+        if 'Items' in response and response['Items']:
+            return {
+                'statusCode': 200,
+                'data': response['Items']
+            }
+        else:
+            return {
+                'statusCode': 404,
+                'status': 'Not Found - No se encontraron cines con los criterios especificados'
+            }
 
     except Exception as e:
         print(f"Error inesperado: {str(e)}")
